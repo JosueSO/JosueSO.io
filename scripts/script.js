74858c0657fc0b240b8d1168ce852b496138d04c
JosueSO <muziko11@hotmail.com>
2017-03-15 12:52:26 -0600

    Blog 2nd Parcial
    Se agrego la pagina de información, icono a las páginas y se validó CSS

---
+++ scripts/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function (){
+    $('#cat-html').click(function (){
+        alert('Función no disponible por el momento');
+    })
+    $('#cat-css').click(function (){
+        alert('Función no disponible por el momento');
+    })
+    $('#cat-js').click(function (){
+        alert('Función no disponible por el momento');
+    })
+})
